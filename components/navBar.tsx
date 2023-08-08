5f02533939f263f61a84eb50b6ec7871e31ee721
kapilsarma2002 <sarmakapil1818@gmail.com>
2023-08-08 20:49:20 +0530

    nav-bar

---
+++ components/navBar.tsx
@@ -0,0 +1,19 @@
+'use client'
+import Link from "next/link"
+
+const links = [
+  { href: '/home', label: 'Home' },
+  { href: '/gita', label: 'Gita' },
+  { href: '/about', label: 'About'}
+]
+
+const NavBar = () => {
+
+  return <div className="border border-black/10 flex flex-row gap-16 text-xl px-3 py-2 rounded-3xl">
+    {links.map(link => (
+      <Link href={link.href} key={link.label}>{link.label}</Link>
+    ))}
+  </div>
+}
+
+export default NavBar
