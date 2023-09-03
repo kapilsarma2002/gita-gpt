b0299720c6134a78cb0b4b33dd4070b3ead73b4b
kapilsarma2002 <sarmakapil1818@gmail.com>
2023-09-03 17:20:15 +0530

    home card redirection

--- components/homeCard.tsx
+++ components/homeCard.tsx
@@ -1,14 +1,35 @@
 'use client'
 
 import { FiArrowRight } from 'react-icons/fi'
+import Link from 'next/link'
+
+const getVerse = (text) => {
+  let ans
+
+  return 1
+} 
 
 const HomeCard = ({ text }) => {
+
+  let link
+
+  if (text == 'Get started') link = '/gita'
+  else {
+    const verse = getVerse(text)
+    link = `/gita/${verse}`
+  }
+
   return (
-    <div className="h-[200px] w-[400px] text-2xl border border-black/10 drop-shadow-md hover:drop-shadow-2xl flex flex-row items-center justify-start pl-8 gap-3 dark:border-white/20">
-      <div>{text}</div>
-      <div className='cursor-pointer hover:pl-0.5'>
-        <FiArrowRight />
-      </div>
+    <div className="h-[200px] w-[400px] text-2xl border border-black/10 drop-shadow-md hover:drop-shadow-2xl  dark:border-white/20">
+      <Link
+        href="/gita"
+        className="flex flex-row items-center justify-start pl-8 pt-8 gap-3"
+      >
+        <div>{text}</div>
+        <div className="cursor-pointer hover:pl-0.5">
+          <FiArrowRight />
+        </div>
+      </Link>
     </div>
   )
 }
