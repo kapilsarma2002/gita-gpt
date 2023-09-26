4846d8887c62ad818e9e6e15ba816f48381e4121
kapilsarma2002 <sarmakapil1818@gmail.com>
2023-09-26 21:34:24 +0530

    responsive nav bar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -10,23 +10,9 @@ const HomeLayout = async ({ children }: any) => {
 
   return (
     <div className="h-screen w-screen relative bg-white text-black dark:bg-slate-900 dark:text-white/80">
-      <div className="h-full">
-        <header className="h-[60px] sm:h-[80px] md:h-[100px] border-b border-black/5 flex flex-row items-center justify-between dark:border-white/20">
-          <div className="pl-6 text-lg sm:text-xl md:text-2xl">Logo</div>
-          <div>
-            <NavBar />
-          </div>
-          <div className="h-full w-[100px] gap-4 px-6 flex items-center justify-end">
-            <div className="text-base sm:text-lg md:text-xl">
-              <Link href={`/profile/${user.userName}`}>{user.userName}</Link>
-            </div>
-            <div className="h-[30px] w-[30px]">
-              <ThemeSwitcher />
-            </div>
-            <UserButton />
-          </div>
-        </header>
-        <div className="h-[calc(100vh-60px)] sm:h-[calc(100vh-80px)] md:h-[calc(100vh-100px)]">
+      <div className="h-full w-full pt-4">
+            <NavBar />       
+        <div className="h-[calc(100vh-60px)] lg:h-[calc(100vh-80px)]">
           {children}
         </div>
       </div>
--- components/navBar.tsx
+++ components/navBar.tsx
@@ -1,24 +1,64 @@
 'use client'
-import Link from "next/link"
+import React, { useState } from 'react'
+import Link from 'next/link'
+import { UserButton } from '@clerk/nextjs'
+import ThemeSwitcher from '@/components/themeSwitcher'
 
 const links = [
   { href: '/home', label: 'Home' },
   { href: '/gita', label: 'Gita' },
   { href: '/bookmarks', label: 'Bookmarks' },
+  { href: '/profile', label: 'Profile'},
   { href: '/about', label: 'About' },
 ]
 
 const NavBar = () => {
+  const [showDropdown, setShowDropdown] = useState(false)
+
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown)
+  }
 
   return (
-    <div className="flex flex-row gap-16 text-xl px-3 py-2 text-black/80 dark:text-white/80">
-      {links.map((link) => (
-        <Link href={link.href} key={link.label}>
-          <div className="hover:text-black/100 dark:hover:text-white/100 transition duration-150 ease-out hover:ease-in">
-            {link.label}
-          </div>
-        </Link>
-      ))}
+    <div className="relative flex justify-between">
+      <div className="lg:flex items-center gap-4 px-6">
+        <div className="pl-6 lg:pl-8">Logo</div>
+      </div>
+      <div className="lg:hidden px-3 py-2">
+        <button
+          onClick={toggleDropdown}
+          className="text-xl text-black/80 dark:text-white/80"
+        >☰ Menu
+        </button>
+      </div>
+      <div className="hidden lg:flex lg:flex-row gap-2 lg:gap-4 lg:text-xl px-3 py-2 text-base lg:text-black/70 dark:text-white/80">
+        {links.map((link) => (
+          <Link href={link.href} key={link.label}>
+            <div className="hover:text-black/100 dark:hover:text-white/100 transition duration-150 ease-out hover:ease-in">
+              {link.label}
+            </div>
+          </Link>
+        ))}
+      </div>
+      <div className="lg:flex items-center gap-4 px-6">
+        <div className="text-lg lg:text-xl">
+          <ThemeSwitcher />
+        </div>
+        {typeof window !== 'undefined' && window.innerWidth >= 768 ? (
+          <UserButton />
+        ) : null}
+      </div>
+      {showDropdown && (
+        <div className="lg:hidden absolute top-full left-0 w-full bg-white border border-black/5 z-10">
+          {links.map((link) => (
+            <Link href={link.href} key={link.label}>
+              <div className="px-3 py-2 hover:bg-gray-200 dark:hover:bg-gray-700">
+                {link.label}
+              </div>
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
