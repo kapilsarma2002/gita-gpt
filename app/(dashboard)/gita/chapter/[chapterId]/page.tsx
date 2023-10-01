cbc16b5c1680f6319dc4f0279ca468270d8f5ed5
kapilsarma2002 <sarmakapil1818@gmail.com>
2023-10-01 21:39:00 +0530

    verse page redirection

--- app/(dashboard)/gita/chapter/[chapterId]/page.tsx
+++ app/(dashboard)/gita/chapter/[chapterId]/page.tsx
@@ -1,8 +1,15 @@
 import verses from '@/utils/verses'
+import Link from 'next/link'
 
 const Chapter = ({ params }) => {
 
   const Id = params.chapterId
+  let x
+  if (Id.length == 2) {
+    x = 9
+  } else {
+    x = 8
+  }
 
   const chapterVerses = verses['Bhagavad-Gita'].filter(
     (verse) => verse.Chapter.slice(8) === Id
@@ -12,13 +19,15 @@ const Chapter = ({ params }) => {
     <div>
       <h1>{Id}</h1>
       {chapterVerses.map((verse) => (
-        <div key={verse['S.No.']}>
-          {/* <h3>{verse.Title}</h3> */}
-          <p>{verse.Verse}</p>
-          {/* <p>{verse['Sanskrit Anuvad']}</p>
-          <p>{verse['Hindi Anuvad']}</p>
-          <p>{verse['English Translation']}</p> */}
-        </div>
+        <Link href={`/gita/chapter/${Id}/verse/${verse.Verse.slice(x)}`} key={verse['S.No.']}>
+          <div >
+            {/* <h3>{verse.Title}</h3> */}
+            <p>{verse.Verse.slice(x)}</p>
+            {/* <p>{verse['Sanskrit Anuvad']}</p>
+            <p>{verse['Hindi Anuvad']}</p>
+            <p>{verse['English Translation']}</p> */}
+          </div>
+        </Link>
       ))}
     </div>
   )
