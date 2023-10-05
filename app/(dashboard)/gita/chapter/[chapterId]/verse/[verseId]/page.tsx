37ab7d1d63a0101df4d82e66c19b40ccca8d75fd
kapilsarma2002 <sarmakapil1818@gmail.com>
2023-10-05 22:52:39 +0530

    redirection issue fix

--- app/(dashboard)/gita/chapter/[chapterId]/verse/[verseId]/page.tsx
+++ app/(dashboard)/gita/chapter/[chapterId]/verse/[verseId]/page.tsx
@@ -3,18 +3,24 @@ import verses from '@/utils/verses'
 const Verse = ({ params }) => {
   const verseId = params.verseId
   const chapterId = params.chapterId
+  let slicingIndex
+  if (chapterId.length == 2) {
+    slicingIndex = 9
+  } else {
+    slicingIndex = 8
+  }
   const verse = verses['Bhagavad-Gita'].find(
-    (verse) => verse.Chapter.slice(8) === chapterId && verse.Verse.slice(8) === verseId
+    (verse) =>
+      verse.Chapter.slice(8) === chapterId &&
+      verse.Verse.slice(slicingIndex) === verseId
   )
-  const sanskritAnuvadLines = verse ? verse['Sanskrit Anuvad'].split('|') : []
 
 
   return (
     <div>
       {verse ? (
         <div>
-          <p>{sanskritAnuvadLines[0]}</p>
-          {sanskritAnuvadLines[1] && <p>{sanskritAnuvadLines[1]}</p>}
+          <p>{verse['Sanskrit Anuvad']}</p>
           <p>{verse['Hindi Anuvad']}</p>
           <p>{verse['English Translation']}</p>
         </div>
