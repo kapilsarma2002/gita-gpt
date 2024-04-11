e635735c9688509bb350d2808d3e9a98562b1a39
kapilsarma2002 <sarmakapil1818@gmail.com>
2024-04-11 18:45:49 +0530

    modified bookmarks text

--- app/(dashboard)/bookmarks/page.tsx
+++ app/(dashboard)/bookmarks/page.tsx
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from 'react'
 import { getBookmarkedVerses } from '@/utils/api'
 import Link from 'next/link'
 
-
 const BookmarkList = () => {
   const [verses, setVerses] = useState(new Set())
 
@@ -28,19 +27,21 @@ const BookmarkList = () => {
 
   return (
     <div className="container mx-auto py-6">
-      <h1 className="text-3xl font-bold mb-6">Bookmarks</h1>
+      <h1 className="text-3xl mb-6">Bookmarks</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         {Array.from(verses).map((verse, index) => (
           <div key={index} className="p-4 bg-white shadow rounded-lg">
             <Link
-              href={`/gita/chapter/${JSON.parse(verse.toString()).chapterId}/verse/${JSON.parse(verse.toString()).verseId}`}
+              href={`/gita/chapter/${
+                JSON.parse(verse.toString()).chapterId
+              }/verse/${JSON.parse(verse.toString()).verseId}`}
             >
-            <h2 className="text-lg font-semibold mb-2">
-              Chapter : {JSON.parse(verse.toString()).chapterId}
-            </h2>
-            <p className="text-gray-600 text-md font-semibold">
-              Verse : {JSON.parse(verse.toString()).verseId}
-            </p>
+              <h2 className="text-2xl mb-2">
+                Chapter : {JSON.parse(verse.toString()).chapterId}
+              </h2>
+              <p className="text-gray-600 text-lg font-semibold">
+                Verse : {JSON.parse(verse.toString()).verseId}
+              </p>
             </Link>
           </div>
         ))}
